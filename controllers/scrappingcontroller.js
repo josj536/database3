eab010a0524459105784db54224dc0535fd92350
josj536 <josjdi48@gmail.com>
2024-08-06 13:49:01 -0500

    Update scrappingcontroller.js

--- controllers/scrappingcontroller.js
+++ controllers/scrappingcontroller.js
@@ -11,7 +11,7 @@ const scrapeAndStoreData = async (req, res) => {
 
     try {
         const browser = await puppeteer.launch({ headless: true,
-            executablePath: '/usr/bin/chromium', // Ajusta esta ruta según el resultado de `which`
+            executablePath: '/usr/bin/chromium-browser', // Ajusta esta ruta según el resultado de `which`
             args: ['--no-sandbox', '--disable-setuid-sandbox'] // Opcional, puede ayudar a evitar problemas de sandboxing});
         const page = await browser.newPage();
         
