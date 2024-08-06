43b48ea026b49689ae1120383cb8fa9ed77a8f9c
josj536 <josjdi48@gmail.com>
2024-08-06 16:42:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { methods as scrapingController } from './controllers/scrappingcontroller.js';
+import dotenv from 'dotenv';
+
+// Configuración de dotenv
+dotenv.config();
+
 const app = express();
+
+// Leer IP y puerto desde variables de entorno
 const port = process.env.PORT || 3000;
+const ip = process.env.IP || '127.0.0.1';
 
 // Middleware
 app.use(bodyParser.json());
@@ -11,6 +19,6 @@ app.use(bodyParser.json());
 app.post('/scrape', scrapingController.scrapeAndStoreData);
 
 // Iniciar servidor
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(port, ip, () => {
+    console.log(`Servidor corriendo en http://${ip}:${port}`);
 });
